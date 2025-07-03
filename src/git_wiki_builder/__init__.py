b259f3cb74f6510113f97ffa0b8f080fbebf93ae
GitHub Action <action@github.com>
2025-07-03 07:20:02 +0000

    Release vv0.1.4

--- src/git_wiki_builder/__init__.py
+++ src/git_wiki_builder/__init__.py
@@ -1,6 +1,6 @@
 """Git Wiki Builder - AI-powered GitHub Wiki generator."""
 
-__version__ = "0.1.3"
+__version__ = "v0.1.4"
 __author__ = "Git Wiki Builder Team"
 __email__ = "support@example.com"
 __description__ = (
